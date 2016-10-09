af583cd284269d8461226920d0af74a8eb2c0963
isdrs <maisam.mohsenian@gmail.com>
2016-10-09 13:47:10 +0330

    Initial Commit

---
+++ nava/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  nava
+//
+//  Created by Mohsenian on 7/18/1395 AP.
+//  Copyright © 1395 manshor. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
